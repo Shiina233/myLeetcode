9c2041b525e8ec68f818d61cffa1bc02822d597f
Shiina233 <1510685040@qq.com>
2020-09-18 20:17:06 +0800

    first commit

---
+++ leetCode100.java
@@ -0,0 +1,34 @@
+package LeetCode;
+//
+public class leetCode100
+{
+    public class TreeNode
+    {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right)
+      {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    //不断比较两个树的对应的节点即可
+    public boolean isSameTree(TreeNode p,TreeNode q)
+    {
+        if(p==null && q==null)
+            return true;
+        else if(p == null || q == null || q.val != p.val)
+            return false;
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+
+
+
+
+
+}
